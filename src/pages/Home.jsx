30c4bc1ae93c9f1a508821fb754c7f3e40c0d6cf
Thecomandate <richard26.fm@gmail.com>
2024-02-22 10:04:11 -0500

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,26 @@ function Home() {
   return (
     <div className="flex bg-blue-600 w-screen h-[90%]">
       {/*Perfil de usuario*/}
-      <div className="bg-amber-400 w-[20%] h-full">
-        <p className="text-black">Perfil</p>
+      <div className="bg-amber-400 w-[20%] h-full flex flex-col gap-5">
+        <button className="flex items-center border-2">
+          <img
+            className="h-10 w-auto flex justify-start mx-4 mt-2 rounded-full border-2"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Logo"
+          />
+          <p className="text-black">UserName</p>
+        </button>
+        <button className="text-black">Configuracion</button>
+        <button className="text-black">Soporte</button>
+        <button className="text-black">Ayuda</button>
+        <button className="text-black">Contacto</button>
+        <button className="text-black">Cerrar Sesion</button>
       </div>
       {/*Contenido*/}
       <div className="bg-red-300 w-[80%] h-full">
         <p>Contenido</p>
+      </div>
     </div>
-  </div>
   );
 }
 
