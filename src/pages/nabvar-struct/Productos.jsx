a48fb179ddfdd4a1d54e0792d93c021debc120bf
DarkLove <magodeozrapid@gmail.com>
2024-02-25 19:46:41 -0500

    Actualizado

--- src/pages/nabvar-struct/Productos.jsx
+++ src/pages/nabvar-struct/Productos.jsx
@@ -1,135 +1,43 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import axios from "axios";
+import MyContext from "../../lib/context";
 
 export default function Productos() {
-  return (
+  const { products, setProducts } = useContext(MyContext);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const { data } = await axios.get("https://fakestoreapi.com/products");
+      setProducts(data);
+    };
+    getProducts();
+  }, []);
+
+  console.log(products);
+
+  return (  
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 justify-between gap-2 h-[50%] w-[90%] mt-3 ml-3 mr-3">
-      <div className="border-2 border-black rounded-lg flex flex-col gap-2">
-        <img
-          className="h-10  flex justify-start mt-2"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt=""
-        />
-        <h3 className="text-[#999999] ml-3">Productos</h3>
-        <p className=" ml-3">Descripcion del producto</p>
-        <h3 className="text-[#999999] ml-3">Precio</h3>
-        <input
-          placeholder="Opiniones del producto"
-          className="bg-[#eee6e6] h-6 w-48 ml-3"
-          type="text"
-        />
-      </div>
-
-      <div className="border-2 border-black rounded-lg flex flex-col gap-2">
-        <img
-          className="h-10  flex justify-start mt-2"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt=""
-        />
-        <h3 className="text-[#999999] ml-3">Productos</h3>
-        <p className=" ml-3">Descripcion del producto</p>
-        <h3 className="text-[#999999] ml-3">Precio</h3>
-        <input
-          placeholder="Opiniones del producto"
-          className="bg-[#eee6e6] h-6 w-48 ml-3"
-          type="text"
-        />
-      </div>
-
-      <div className="border-2 border-black rounded-lg flex flex-col gap-2">
-        <img
-          className="h-10  flex justify-start mt-2"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt=""
-        />
-        <h3 className="text-[#999999] ml-3">Productos</h3>
-        <p className=" ml-3">Descripcion del producto</p>
-        <h3 className="text-[#999999] ml-3">Precio</h3>
-        <input
-          placeholder="Opiniones del producto"
-          className="bg-[#eee6e6] h-6 w-48 ml-3"
-          type="text"
-        />
-      </div>
-
-      <div className="border-2 border-black rounded-lg flex flex-col gap-2">
-        <img
-          className="h-10  flex justify-start mt-2"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt=""
-        />
-        <h3 className="text-[#999999] ml-3">Productos</h3>
-        <p className=" ml-3">Descripcion del producto</p>
-        <h3 className="text-[#999999] ml-3">Precio</h3>
-        <input
-          placeholder="Opiniones del producto"
-          className="bg-[#eee6e6] h-6 w-48 ml-3"
-          type="text"
-        />
-      </div>
-
-      <div className="border-2 border-black rounded-lg flex flex-col gap-2">
-        <img
-          className="h-10  flex justify-start mt-2"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt=""
-        />
-        <h3 className="text-[#999999] ml-3">Productos</h3>
-        <p className=" ml-3">Descripcion del producto</p>
-        <h3 className="text-[#999999] ml-3">Precio</h3>
-        <input
-          placeholder="Opiniones del producto"
-          className="bg-[#eee6e6] h-6 w-48 ml-3"
-          type="text"
-        />
-      </div>
-
-      <div className="border-2 border-black rounded-lg flex flex-col gap-2">
-        <img
-          className="h-10  flex justify-start mt-2"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt=""
-        />
-        <h3 className="text-[#999999] ml-3">Productos</h3>
-        <p className=" ml-3">Descripcion del producto</p>
-        <h3 className="text-[#999999] ml-3">Precio</h3>
-        <input
-          placeholder="Opiniones del producto"
-          className="bg-[#eee6e6] h-6 w-48 ml-3"
-          type="text"
-        />
-      </div>
-
-      <div className="border-2 border-black rounded-lg flex flex-col gap-2">
-        <img
-          className="h-10  flex justify-start mt-2"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt=""
-        />
-        <h3 className="text-[#999999] ml-3">Productos</h3>
-        <p className=" ml-3">Descripcion del producto</p>
-        <h3 className="text-[#999999] ml-3">Precio</h3>
-        <input
-          placeholder="Opiniones del producto"
-          className="bg-[#eee6e6] h-6 w-48 ml-3"
-          type="text"
-        />
-      </div>
-
-      <div className="border-2 border-black rounded-lg flex flex-col gap-2">
-        <img
-          className="h-10  flex justify-start mt-2"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt=""
-        />
-        <h3 className="text-[#999999] ml-3">Productos</h3>
-        <p className=" ml-3">Descripcion del producto</p>
-        <h3 className="text-[#999999] ml-3">Precio</h3>
-        <input
-          placeholder="Opiniones del producto"
-          className="bg-[#eee6e6] h-6 w-48 ml-3"
-          type="text"
-        />
-      </div>
+      {products.map((product) => (
+        <div
+          key={product.id}
+          className="border-2 border-black rounded-lg flex flex-col gap-2"
+        >
+          <img
+            className="h-10  flex justify-start mt-2 mx-auto"
+            src={product.image}
+            width={100}
+            height={100}
+          />
+          <h3 className="text-[#999999] ml-3">{product.title}</h3>
+          <p className=" ml-3">{product.description}</p>
+          <h3 className="text-[#999999] ml-3">{product.price}</h3>
+          <input
+            placeholder="Opiniones del producto"
+            className="bg-[#eee6e6] h-6 w-48 ml-3"
+            type="text"
+          />
+        </div>
+      ))}
     </div>
   );
 }
